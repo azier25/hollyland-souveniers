f21beb3a45c04168aac72c0e259f692cb485cffc
Montaser Alfrookh <montaser.alfrookh91@gmail.com>
2020-12-25 12:32:01 +0200

    create admin panel

--- hollylandSouveniers/hollylandApp/models.py
+++ hollylandSouveniers/hollylandApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=190)
+    image = models.ImageField(upload_to='image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=190)
+    image = models.ImageField(upload_to='image')
+    price = models.FloatField()
+    category = models.ForeignKey(Category, related_name="category",  on_delete=models.CASCADE)
+    description = models.TextField(null = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- hollylandSouveniers/hollylandApp/views.py
+++ hollylandSouveniers/hollylandApp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
-    return render(request,'index.html')
+    categories = Category.objects.all()
+    return render(request,'index.html',{'categories':categories})
 
 def signup(request):
     return render(request,'signup.html')
 
 def items(request):
     return render(request,'items.html')
+
+
